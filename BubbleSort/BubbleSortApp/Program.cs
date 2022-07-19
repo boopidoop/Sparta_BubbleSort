b262721bbbb1956bbdb7a17277c307a0b25e7488
boopidoop <daniel.summerside@yahoo.com>
2022-07-19 16:24:06 +0100

    First BubbleSort Version

--- BubbleSort/BubbleSortApp/Program.cs
+++ BubbleSort/BubbleSortApp/Program.cs
@@ -2,5 +2,31 @@
 
 public class Program
 {
+    public static int[] BubbleSort(int[] ints)
+    {
+        bool sorted = false;
+        while (!sorted)
+        {
+            // Switch if next element is lower
+            for (int i = 0; i < ints.Length - 1; i++)
+            {
+                if (ints[i] > ints[i + 1])
+                {
+                    int swap = ints[i];
+                    ints[i] = ints[i + 1];
+                    ints[i + 1] = swap;
+                }
+            }
 
+            // Check if list is sorted
+            sorted = true;
+            for (int i = 0; i < ints.Length - 1; i++)
+            {
+                if (ints[i] > ints[i + 1])
+                    sorted = false;
+            }
+        }
+
+        return ints;
+    }
 }
