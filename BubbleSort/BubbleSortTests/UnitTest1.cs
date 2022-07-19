3236437173ec5b07335f0f955a66cefa73c7402b
LabaDev <labalimbu360@gmail.com>
2022-07-19 16:57:23 +0100

    Added 3 tests

--- BubbleSort/BubbleSortTests/UnitTest1.cs
+++ BubbleSort/BubbleSortTests/UnitTest1.cs
@@ -32,5 +32,26 @@ namespace BubbleSortTests
             var array3 = new int[5];
             Assert.That(Program.BubbleSort(array3), Is.EqualTo(array3));
         }
+
+        [Test]
+        public void GivenRandomNumbers_BubbleSort_Returns0To5Ascending()
+        {
+            var array4 = new int[] { 4, 3, 5, 2, 0, 1 };
+            Assert.That(Program.BubbleSort(array4), Is.EqualTo(new int[] { 0, 1, 2, 3, 4, 5 }));
+        }
+
+        [Test]
+        public void GivenRandomNumbers_BubbleSort_Returns0To10Ascending()
+        {
+            var array5= new int[] { 8,4, 3, 5, 9, 7, 6,2, 0, 1 };
+            Assert.That(Program.BubbleSort(array5), Is.EqualTo(new int[] { 0, 1, 2, 3, 4, 5, 6, 7,8,9, }));
+        }
+
+        [Test]
+        public void GivenRandomNumbersWithin100_BubbleSort_ReturnsAscendingNumbers()
+        {
+            var array5 = new int[] { 80, 4, 3, 50, 97, 7, 65, 2, 0, 16 };
+            Assert.That(Program.BubbleSort(array5), Is.EqualTo(new int[] { 0, 2, 3, 4, 7, 16, 50, 65, 80, 97 }));
+        }
     }
 }
